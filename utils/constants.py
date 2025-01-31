6e08477724f83f795228f54d987371a33796b2c9
incentia <78892324+incentia@users.noreply.github.com>
2025-01-31 12:51:13 +0800

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -8,24 +8,39 @@ TOPIC_TYPES = {
 # Device States
 OPERATION_MODES = {
     "0": "关机",
-    "1": "采暖关闭",
-    "2": "休眠",
-    "3": "冬季普通",
-    "4": "快速热水",
-    "B": "采暖节能",
-    "23": "采暖预约",
-    "13": "采暖外出",
-    "43": "快速采暖",
-    "4B": "快速采暖/节能",
-    "53": "快速采暖/外出",
-    "63": "快速采暖/预约"
+    "A0":  "普通模式",
+    "90":  "淋浴模式",
+    "88":  "水温按摩模式",
+    "82":  "低温模式",
+    "81":  "厨房模式",
+    "84":  "浴缸模式",
+    "E0":  "普通模式+循环",
+    "D0":  "淋浴模式+循环",
+    "C2":  "低温模式+循环",
+    "C1":  "厨房模式+循环"
+    
+}
+
+CYCLE_MODE = {
+    "0": "标准循环",
+    "1": "舒适循环",
+    "2": "节能循环"
 }
 
 BURNING_STATES = {
-    "30": "待机中",
-    "31": "烧水中",
-    "32": "燃烧中",
-    "33": "异常"
+    "0": "待机中",
+    "1": "燃烧中",
+    "2": "异常"
+}
+
+ONLINE_STATUS = {
+    "0": "离线",
+    "1": "在线",
+}
+
+SWITCH_STATUS = {
+    "0": "关",
+    "1": "开"
 }
 
 # Message Types
@@ -45,9 +60,15 @@ STATE_PARAMETERS = {
     'burningState',
     'hotWaterTempSetting',
     'heatingTempSettingNM',
-    'heatingTempSettingHES'
-}
+    'heatingTempSettingHES',
+    'cycleReservationSetting',
+    'temporaryCycleInsulationSetting',
+    'childLock',
+    'priority',
+    'cycleModeSetting'
+    }
 
+CURRENT_HOTWATER_TEMP = 39
 
 HOST = "https://iot.rinnai.com.cn/app"
 LOGIN_URL = f"{HOST}/V1/login"
